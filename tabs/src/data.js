ecc24a6e8d57ca0d95f745e2e4c3d19c8a3c9267
Brancorpse <lim.machina@gmail.com>
2022-08-25 03:18:58 -0300

    Add files via upload

---
+++ tabs/src/data.js
@@ -0,0 +1,38 @@
+const data = [
+
+    {
+        id: 1,
+        title: "Full Stack Developer",
+        date: "Dezembro 2015 - Atual",
+        duties: ["Desenvolver aplicações, atuando tanto em back quanto front-end",
+                "Gerenciar projeto e time de programdores."
+
+        ],
+        company: "TOMMY"
+    },
+
+    {
+        id: 2,
+        title: "Desenvolvedor Front-End",
+        date: "Maio 2015 - Atual",
+        duties: ["Desenvolver aplicações em front-end, usando Javascript, CSS e React",
+                "Desenvolver interfaces mobile"
+
+        ],
+        company: "BIGDROP"
+    },
+
+    {
+        id: 3,
+        title: "Estágio em Engenharia",
+        date: "Dezembro 2015 - Julho 2015",
+        duties: ["Desenvolver modelos de interação de software e documentação",
+                "Atuar com stakeholders sobre requisitos do negócio"
+
+        ],
+        company: "TOMMY"
+    }
+
+]
+
+export default data;
